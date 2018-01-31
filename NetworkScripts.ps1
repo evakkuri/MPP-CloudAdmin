139af9e6cc02a03e2431bbf29ca4ba1d3835a944
evakkuri <elias.vakkuri@gmail.com>
2018-01-31 21:30:40 +0200

    Added networkscripts

---
+++ NetworkScripts.ps1
@@ -0,0 +1,13 @@
+﻿Get-AzureRmVM
+
+Get-AzureRmNetworkInterface -ResourceGroupName "SERVER2012R2-RG" -Name "server-01927"
+
+$NIC = Get-AzureRmNetworkInterface -Name "server-01927" -ResourceGroupName "SERVER2012R2-RG"
+
+$NIC.IpConfigurations[0].PrivateIpAllocationMethod = "Static"
+
+$NIC.IpConfigurations[0].PrivateIpAddress = "10.0.0.50"
+
+Set-AzureRmNetworkInterface -NetworkInterface $NIC
+
+Get-AzureRmVM -Name "SERVER-01" -ResourceGroupName “SERVER2012R2-RG” | Stop-AzureRmVM
